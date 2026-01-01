7bfa22cfa614e1fa5653929811634a166a31270d
Bhavanti <bhavanti.official@gmail.com>
2026-01-01 23:27:41 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (47.64%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -1,13 +1,14 @@
-    const plusOne = function(digits) {
-    for(let i=digits.length-1; i>=0; i--){
-        if(digits[i]<9){
-            digits[i]=digits[i]+1;
-            return digits;
-        }else{
-            digits[i]=0;
-        }
+const plusOne = function(digits) {
+   let b=digits[digits.length-1]+1;
+   let a=b%10;
+   for(let i=digits.length-2; i>=0; i--){
+    b=(digits[i]+Math.trunc(b/10));
+    digits[i]=b%10;
+   }
+    if(Math.trunc(b/10)===1){
+        digits.unshift(1);
     }
-    digits.unshift(1);
-    return digits;
-    };
-    console.log(plusOne([1,2,3]));
+   digits[digits.length-1]=a;
+   return digits;
+};
+console.log(plusOne([9,9,9]));
